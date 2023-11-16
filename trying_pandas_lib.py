0a364d32831a790485823ed65f719f7b665ccc01
Mike <mike@Zipcoders-MacBook-Pro-83.local>
2023-11-16 15:09:16 -0500

    trying more pandas functions & tabulate formatter

--- trying_pandas_lib.py
+++ trying_pandas_lib.py
@@ -1,13 +1,58 @@
 import pandas as pd
+from tabulate import tabulate
+
+
+# get tabulate module -> has a bunch of format for outputting final data
 
 # For beginners purpose, assume/use clean, whole dataset
 csv_file = pd.read_csv('ice_cream.csv')
 
-# Headers - Column names from CSV
-# headers = pd.DataFrame(csv_file.columns)
-headers = ['Student ID', 'Gender', 'Favorite Ice Cream', 'Video Game Score', 'Puzzle Score']
+# csv_file.info() -> either that or print will print out all the values
+# ic.head() -> can put an integer in there, or default 5 - gets top X columns/rows of data
 
-# Each column is now a pandas series
+student_ids = csv_file['id']
+genders = csv_file['female']
+favorite_ice_cream = csv_file['ice_cream']
+video_game_scores = csv_file['video']
+puzzle_scores = csv_file['puzzle']
+
+# Headers - Column names from CSV
+# Grabs heading column and renames it, changes it to list
+headers = pd.DataFrame(csv_file.columns)
+headers = ['Student ID', 'Gender', 'Favorite Ice Cream', 'Video Game Score', 'Puzzle Score']
+# headers.rename() -> use this to modify the same original series or dataframe
+csv_file.columns = ['Student_ID', 'Gender', 'Favorite_Ice_Cream', 'Video_Game_Score', 'Puzzle_Score']
+print(csv_file.columns)
+
+# Renaming data in data frame
+# Replace the values in gender ('female' column) with 0 = Male and 1 = Female
+csv_file["Gender"] = csv_file["Gender"].replace({0: "male", 1: "female"})
+
+# Renaming is for presenting data in understandable way
+# Replace the values in gender ('female' column) with 0 = Male and 1 = Female
+csv_file["Favorite_Ice_Cream"] = csv_file["Favorite_Ice_Cream"].replace({3: "Strawberry", 2: "Chocolate", 1: "Vanilla"})
+
+# For tabulate, set changed DataFrame to a fresh variable
+for_tabulate_csv_file = pd.DataFrame(csv_file)
+
+# For tabulate, make a method to call to print it out,  it to a function
+def print_tabulate_table(df, format):
+    print(tabulate(df, headers="Keys", tablefmt=format))
+
+
+# Avg video game score for each gender
+avg_gender_video_score = csv_file.groupby("Gender").Video_Game_Score.mean().reset_index()
+print(avg_gender_video_score)
+
+# Avg video game score for each gender by flavor
+avg_gender_video_score = csv_file.groupby(["Gender", "Favorite_Ice_Cream"]).Video_Game_Score.mean().reset_index()
+print(avg_gender_video_score)
+
+
+
+
+"""
+# TO USE - change the names to the renamed column name if declaring after changing columns...duh Mike
 student_ids = csv_file['id']
 genders = csv_file['female']
 favorite_ice_cream = csv_file['ice_cream']
@@ -20,6 +65,8 @@ mean_vgs_male_vanilla = male_vanilla.video.mean()
 mean_ps_male_vanilla = male_vanilla.puzzle.mean()
 print(mean_vgs_male_vanilla)
 print(mean_ps_male_vanilla)
+"""
+
 
 """
 # Not Sure - Series or DataFrame of mean score for each game
