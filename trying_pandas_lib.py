bfc094fc0807fa3a698dba84d5650d41694f786c
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-16 22:09:06 -0500

    more pandas stuff

--- trying_pandas_lib.py
+++ trying_pandas_lib.py
@@ -2,54 +2,92 @@ import pandas as pd
 from tabulate import tabulate
 
 
-# get tabulate module -> has a bunch of format for outputting final data
+"""
+EDA on data set of students who selected a favorite ice cream and were scored on a puzzle and video game test
+Using pandas (DataFrames & Series for data analysis) and tabulate (output formatting)
+There will be heavy commenting throughout, spreading this out over a few days and need to keep continuity
+The rtf supplied provides the key info for what columns, values and types are in the data set
+"""
 
 # For beginners purpose, assume/use clean, whole dataset
 csv_file = pd.read_csv('ice_cream.csv')
+csv_file2 = pd.read_csv('ice_cream.csv')
 
+# Helpful methods
 # csv_file.info() -> either that or print will print out all the values
-# ic.head() -> can put an integer in there, or default 5 - gets top X columns/rows of data
+# csv_file.head() -> can put an integer in there, or defaults 5 - gets top X columns/rows of data
 
+# I can make each column of data a Series object, analysis/manipulation methods provided by pandas
+# This might be useful...but I paused this idea to start simple with the DataFrame object
 student_ids = csv_file['id']
 genders = csv_file['female']
 favorite_ice_cream = csv_file['ice_cream']
 video_game_scores = csv_file['video']
 puzzle_scores = csv_file['puzzle']
 
-# Headers - Column names from CSV
-# Grabs heading column and renames it, changes it to list
-headers = pd.DataFrame(csv_file.columns)
-headers = ['Student ID', 'Gender', 'Favorite Ice Cream', 'Video Game Score', 'Puzzle Score']
-# headers.rename() -> use this to modify the same original series or dataframe
+# Headers - 0th Column from CSV file
+# I want to give them more meaningful names, a list might work, but pandas has some methods as well
+# headers = pd.DataFrame(csv_file.columns)
+# headers = ['Student ID', 'Gender', 'Favorite Ice Cream', 'Video Game Score', 'Puzzle Score']
+
+# headers.rename() -> This will modify the same original series or dataframe, not make it a new list.
+# The below function .columns grabs row 0th from csv_file and assigns new values.
 csv_file.columns = ['Student_ID', 'Gender', 'Favorite_Ice_Cream', 'Video_Game_Score', 'Puzzle_Score']
-print(csv_file.columns)
 
-# Renaming data in data frame
+# DataFrame - Reassigning data values, providing better context for humans
 # Replace the values in gender ('female' column) with 0 = Male and 1 = Female
-csv_file["Gender"] = csv_file["Gender"].replace({0: "male", 1: "female"})
+# csv_file["Gender"] = csv_file["Gender"].replace({0: "male", 1: "female"})
 
-# Renaming is for presenting data in understandable way
-# Replace the values in gender ('female' column) with 0 = Male and 1 = Female
-csv_file["Favorite_Ice_Cream"] = csv_file["Favorite_Ice_Cream"].replace({3: "Strawberry", 2: "Chocolate", 1: "Vanilla"})
+# Replace the values in Favorite_Ice_Cream ('ice_cream' column) with vanilla, chocolate and strawberry
+# csv_file["Favorite_Ice_Cream"] = csv_file["Favorite_Ice_Cream"].replace({3: "Strawberry", 2: "Chocolate", 1: "Vanilla"})
 
 # For tabulate, set changed DataFrame to a fresh variable
-for_tabulate_csv_file = pd.DataFrame(csv_file)
+# This is one way, but uses another variable: for_tabulate_csv_file = pd.DataFrame(csv_file)
 
 # For tabulate, make a method to call to print it out,  it to a function
 def print_tabulate_table(df, format):
-    print(tabulate(df, headers="Keys", tablefmt=format))
+
+    print(tabulate(df, headers="keys", tablefmt=format))
 
 
-# Avg video game score for each gender
-avg_gender_video_score = csv_file.groupby("Gender").Video_Game_Score.mean().reset_index()
-print(avg_gender_video_score)
+# Print out DataFrame of CSV file with renamed keys (column 0) and values
+# print_tabulate_table(csv_file, "simple")
 
+# Variables, Dependent: Favorite_Ice_Cream, Independent: Gender, Puzzle_Score, Game_Score
+# A dependent variable would be one that should not change...narrows what I should cacluate
+# An independent variable is the one that varies...which can be done with favorite ice cream
+
+# Try one mean comparison for favorite ice cream and mean score..
+# This means: Can a students favorite ice cream predict a better average video game score
+avg_ic_video_score = csv_file.groupby("Favorite_Ice_Cream").Video_Game_Score.mean().reset_index()
+print_tabulate_table(avg_ic_video_score, "simple")
+print('*' * 20)
+
+avg_ic_puzzle_score = csv_file.groupby("Favorite_Ice_Cream").Puzzle_Score.mean().reset_index()
+print_tabulate_table(avg_ic_puzzle_score, "simple")
+print('*' * 20)
+
+avg_ic_gender = csv_file.groupby("Favorite_Ice_Cream").Gender.mean().reset_index()
+print_tabulate_table(avg_ic_gender, "simple")
+print('*' * 20)
+
+# What other statistics should I check?  Std dev? norm dist?
+
+
+"""
+Example from Imir white boarding
 # Avg video game score for each gender by flavor
 avg_gender_video_score = csv_file.groupby(["Gender", "Favorite_Ice_Cream"]).Video_Game_Score.mean().reset_index()
 print(avg_gender_video_score)
+"""
 
 
-
+"""
+Probably don't need this - we aren't after score by gender.
+# Avg video game score for each gender
+avg_gender_video_score = csv_file.groupby("Gender").Video_Game_Score.mean().reset_index()
+print(avg_gender_video_score)
+"""
 
 """
 # TO USE - change the names to the renamed column name if declaring after changing columns...duh Mike
